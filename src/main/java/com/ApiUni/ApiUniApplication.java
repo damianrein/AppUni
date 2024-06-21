4c964422c54d132dd5ba2a092c2d586648bbd436
damianrein <damianrein464@gmail.com>
2024-06-20 21:23:56 -0300

    -

--- src/main/java/com/ApiUni/ApiUniApplication.java
+++ src/main/java/com/ApiUni/ApiUniApplication.java
@@ -14,9 +14,9 @@ public class ApiUniApplication {
 /*
 - Listar todas las investigaciones en curso.------------------------------------------------------>Lista
 - Listar los premios recibidos, durante el año 2023.---------------------------------------------->Lista
-- Los datos de los investigadores que pertenezcan a la facultad de ciencias sociales.
-- Que investigadores no han formado parte de ninguna investigación en los últimos 3 años.
-- Todos los investigadores categoría 1 ó 2 con título de doctor.
-- Las sedes que no posean grupo de investigación activo.
-- Temas de investigación en los últimos 5 años.
+- Los datos de los investigadores que pertenezcan a la facultad de ciencias sociales.------------->
+- Que investigadores no han formado parte de ninguna investigación en los últimos 3 años.--------->
+- Todos los investigadores categoría 1 ó 2 con título de doctor.---------------------------------->
+- Las sedes que no posean grupo de investigación activo.------------------------------------------>
+- Temas de investigación en los últimos 5 años.--------------------------------------------------->
 */
--- src/main/java/com/ApiUni/model/Sede.java
+++ src/main/java/com/ApiUni/model/Sede.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 
 @Entity
 public class Sede {
@@ -14,4 +15,22 @@ public class Sede {
 	private Long id;
 	@ManyToOne
 	private Facultad facultad;
+	@OneToOne
+	private Laboratorio laboratorio;
+	
+	public Facultad getFacultad() {
+		return facultad;
+	}
+	public void setFacultad(Facultad facultad) {
+		this.facultad = facultad;
+	}
+	public Laboratorio getLaboratorio() {
+		return laboratorio;
+	}
+	public void setLaboratorio(Laboratorio laboratorio) {
+		this.laboratorio = laboratorio;
+	}
+	public Long getId() {
+		return id;
+	}	
 }
