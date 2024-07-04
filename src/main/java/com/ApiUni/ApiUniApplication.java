6a7cda9723cd6498e6a66b64e96a1edc6a630b2e
damianrein <damianrein464@gmail.com>
2024-07-04 18:00:11 -0300

    inicial

--- src/main/java/com/ApiUni/ApiUniApplication.java
+++ src/main/java/com/ApiUni/ApiUniApplication.java
@@ -16,7 +16,7 @@ public class ApiUniApplication {
 - Listar los premios recibidos, durante el año 2023.---------------------------------------------->Lista
 - Los datos de los investigadores que pertenezcan a la facultad de ciencias sociales.------------->
 - Que investigadores no han formado parte de ninguna investigación en los últimos 3 años.--------->
-- Todos los investigadores categoría 1 ó 2 con título de doctor.---------------------------------->
+- Todos los investigadores categoría 1 ó 2 con título de doctor.---------------------------------->Lista
 - Las sedes que no posean grupo de investigación activo.------------------------------------------>Lista
 - Temas de investigación en los últimos 5 años.--------------------------------------------------->Lista
 */
---
+++ src/main/java/com/ApiUni/model/Usuario.java
@@ -0,0 +1,53 @@
+package com.ApiUni.model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario implements UserDetails{
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	private String password;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getPassword() {
+		return password;
+	}
+	@Override
+	public String getUsername() {
+		return email;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return false;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
